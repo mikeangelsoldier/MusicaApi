4adc3a90192c6b86230a0b61a2166809382bd7b5
mikeangelsoldier <mikeangelsoldier1@gmail.com>
2023-07-18 21:16:42 -0600

    Modelo Cancion

---
+++ Models/Cancion.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace MusicaApi.Models
+{
+    public class Cancion
+    {
+
+
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public string? Genero { get; set; }
+
+        public string? Banda { get; set; }
+
+        public string? Discografia { get; set; }
+
+    }
+}
